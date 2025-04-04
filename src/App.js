69374a19d15eae2404ee11e528b16d6fabbe902d
Avantika <avantika4568@gmail.com>
2025-04-04 15:24:18 +0530

    changed heading

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test Project
         </p>
         <a
           className="App-link"
